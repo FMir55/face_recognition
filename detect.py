e476a528d621d1ead362713a029abb82828f6611
Hank <s1006005@gmail.com>
2022-06-19 18:44:10 +0800

    v3

--- detect.py
+++ detect.py
@@ -10,14 +10,6 @@ class Args:
     threshold = 0.1
 
 def main():
-    '''
-    parser.add_argument('--labels', help='label file path',
-                        default=os.path.join(default_model_dir, default_labels))
-    parser.add_argument('--top_k', type=int, default=3,
-                        help='number of categories with highest score to display')
-    parser.add_argument('--threshold', type=float, default=0.1,
-                        help='classifier score threshold')
-    '''
     args = Args()
     interpreter = make_interpreter(args.model)
     interpreter.allocate_tensors()
@@ -34,7 +26,7 @@ def main():
         cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
         cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
         run_inference(interpreter, cv2_im_rgb.tobytes())
-        objs = get_objects(interpreter, args.threshold)[:args.top_k]
+        objs = get_objects(interpreter, args.threshold)
         cv2_im = append_objs_to_img(cv2_im, inference_size, objs)
         cv2.imshow('frame', cv2_im)
         if cv2.waitKey(1) & 0xFF == ord('q'):
