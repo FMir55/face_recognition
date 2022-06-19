e70e0670a1150759dddd249392668199eec944f6
Hank <s1006005@gmail.com>
2022-06-19 19:29:37 +0800

    ccc

--- detect.py
+++ detect.py
@@ -1,4 +1,13 @@
 import cv2
+import numpy as np
+from deepface import DeepFace
+from deepface.commons import distance as dst
+from deepface.commons import functions
+from deepface.extendedmodels import Age
+
+'''
+from deepface.detectors import FaceDetector
+'''
 from pycoral.adapters.common import input_size
 from pycoral.adapters.detect import get_objects
 from pycoral.utils.edgetpu import make_interpreter, run_inference
@@ -8,9 +17,20 @@ class Args:
     model = 'all_models/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite'
     camera_idx = 2
     threshold = 0.1
+    emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+def get_models():
+    return (
+        DeepFace.build_model('Emotion')
+        age_model = DeepFace.build_model('Age')
+        gender_model = DeepFace.build_model('Gender')
+    )
+
+args = Args()
+emotion_model, age_model, gender_model = get_models()
 
 def main():
-    args = Args()
+    
     interpreter = make_interpreter(args.model)
     interpreter.allocate_tensors()
     inference_size = input_size(interpreter)
@@ -29,13 +49,9 @@ def main():
         cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
         run_inference(interpreter, cv2_im_rgb.tobytes())
         objs = get_objects(interpreter, args.threshold)
-        cv2_im = append_objs_to_img(cv2_im, inference_size, objs)
+        cv2_im = append_objs_to_img(cv2_im, inference_size, objs, args.detector_backend)
 
         res = "test"
-        '''
-        cv2.namedWindow(res, cv2.WND_PROP_FULLSCREEN)          
-        cv2.setWindowProperty(res, cv2.WND_PROP_FULLSCREEN, cv2.CV_WINDOW_FULLSCREEN)
-        '''
         cv2.imshow(res, cv2_im)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -43,7 +59,28 @@ def main():
     cap.release()
     cv2.destroyAllWindows()
 
+def get_face_attr(crop_bgr):
+    gray_img = functions.preprocess_face(img = crop_bgr, target_size = (48, 48), grayscale = True, enforce_detection = False, detector_backend = 'ssd')
+    face_224 = functions.preprocess_face(img = crop_bgr, target_size = (224, 224), grayscale = False, enforce_detection = False, detector_backend = 'ssd')
+
+    # emotion
+    emotion_predictions = emotion_model.predict(gray_img)[0,:]
+    emotion = args.emotion_labels[np.argmax(emotion_predictions)]
+
+    # age
+    age_predictions = age_model.predict(face_224)[0,:]
+    apparent_age = Age.findApparentAge(age_predictions)
+    age = int(round(apparent_age))
+
+    # gender
+    gender_prediction = gender_model.predict(face_224)[0,:]
+    idx = np.argmax(gender_prediction)
+    gender = "Female" if idx == 0 else "Male"
+
+    return emotion, age, gender
+
 def append_objs_to_img(cv2_im, inference_size, objs):
+    cv2_im_clean = cv2_im.copy()
     height, width, channels = cv2_im.shape
     scale_x, scale_y = width / inference_size[0], height / inference_size[1]
     for obj in objs:
@@ -54,8 +91,15 @@ def append_objs_to_img(cv2_im, inference_size, objs):
         percent = int(100 * obj.score)
         label = '{}% {}'.format(percent, 'face')
 
+        crop_bgr = cv2_im_clean[y0:y1, x0:x1]
+        emotion, age, gender = get_face_attr(crop_bgr)
+
+        # draw
+        attr = f"{gender}, {age}y, {emotion}"
         cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
-        cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
+        # cv2.putText(bbox_array, text, (left+5, top-10), 0, 0.6, color_text, thickness=2)
+        cv2.putText(cv2_im, attr, (x+5, y-10), 0, 0.6, (0, 0, 255), thickness=2)
+        cv2_im = cv2.putText(cv2_im, attr, (x0, y0+30),
                              cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
     return cv2_im
 
