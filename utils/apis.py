63411fa72ba7fb3d863bbc5e451fc3a0a86710f3
Hank <s1006005@gmail.com>
2022-06-21 22:57:31 +0800

    ddgg

--- utils/apis.py
+++ utils/apis.py
@@ -5,18 +5,18 @@ import requests
 
 
 def get_face_info(img_bgr, fname='sample.jpg'):
-  _, encoded_image = cv2.imencode('.jpg', img_bgr)
+    _, encoded_image = cv2.imencode('.jpg', img_bgr)
 
-  files = {
-      "image": (fname, encoded_image.tobytes())
-  }
+    files = {
+        "image": (fname, encoded_image.tobytes())
+    }
 
-  response = requests.post(
-      "https://heartrate.ap-mic.com/get_face_info",
-      files=files
-  )
-  
-  return response.json()
+    response = requests.post(
+        "https://heartrate.ap-mic.com/get_face_info",
+        files=files
+    )
+    
+    return response.json()
 
 def get_embedding(img, fname = 'sample.jpg'):
     _, encoded_image = cv2.imencode('.jpg', img)
@@ -33,18 +33,18 @@ def get_embedding(img, fname = 'sample.jpg'):
     return np.array(response.json()['embedding'])
 
 def get_embeddings(suspects, distance_metric="cosine"):
-  embeddings = []
-  for suspect in suspects:
-    img = cv2.imread(suspect)
-    embeddings.append(
-        (
-            suspect,
-            get_embedding(img)
+    embeddings = []
+    for suspect in suspects:
+        img = cv2.imread(suspect)
+        embeddings.append(
+            (
+                suspect,
+                get_embedding(img)
+            )
         )
-    )
-    df = pd.DataFrame(embeddings, columns = ['suspect', 'embedding_template'])
-    df['distance_metric'] = distance_metric
-    return df
+        df = pd.DataFrame(embeddings, columns = ['suspect', 'embedding_template'])
+        df['distance_metric'] = distance_metric
+        return df
 
 def get_attr(id2info, crop_bgr):
     if id in id2info: 
