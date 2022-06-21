31597fd15786eed393705098c76109425d4bf67c
Hank <s1006005@gmail.com>
2022-06-22 02:18:53 +0800

    aad

--- utils/inference.py
+++ utils/inference.py
@@ -31,7 +31,7 @@ def prewhiten(x):
 def inference_embedding(cv2_im, interpreter_emb, inference_size_emb):
     cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
     cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size_emb)
-    cv2_im_rgb = prewhiten(cv2_im_rgb)
+    # cv2_im_rgb = prewhiten(cv2_im_rgb)
     run_inference(interpreter_emb, cv2_im_rgb.tobytes())
     print(output_tensor(interpreter_emb, -1)[0])
     return output_tensor(interpreter_emb, -1)[0]
