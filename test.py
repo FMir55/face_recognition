389a8eeec2601b96e9acb57f3b68e8b43f64f451
Hank <s1006005@gmail.com>
2022-06-22 02:42:21 +0800

    fgg

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from pycoral.adapters.common import input_size, output_tensor
 from pycoral.utils.edgetpu import make_interpreter, run_inference
 
@@ -7,6 +8,22 @@ class Args:
     model = '../all_models/facenet.tflite'
 
 args = Args()
+
+def prewhiten(x):
+    if x.ndim == 4:
+        axis = (1, 2, 3)
+        size = x[0].size
+    elif x.ndim == 3:
+        axis = (0, 1, 2)
+        size = x.size
+    else:
+        raise ValueError('Dimension should be 3 or 4')
+
+    mean = np.mean(x, axis=axis, keepdims=True)
+    std = np.std(x, axis=axis, keepdims=True)
+    std_adj = np.maximum(std, 1.0/np.sqrt(size))
+    y = (x - mean) / std_adj
+    return y
 
 def main():
     interpreter = make_interpreter(args.model)
@@ -17,11 +34,13 @@ def main():
     cv2_im = cv2.imread('face_db/a/a0.jpg')
     cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
     cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
+    aligned_images =  prewhiten(cv2_im_rgb[np.newaxis])
+
     '''
     import numpy as np
     cv2_im_rgb = np.ones((160, 160, 3)) * 125
     '''
-    run_inference(interpreter, cv2_im_rgb.tobytes())
+    run_inference(interpreter, aligned_images.tobytes())
     embedding = output_tensor(interpreter, -1)
     print(embedding)
 
