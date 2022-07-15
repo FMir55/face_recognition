f4a5c451aee54e27549251d55d8dd013023f9505
Hank <s1006005@gmail.com>
2022-07-15 17:24:03 +0800

    ccc

--- detect_v4.py
+++ detect_v4.py
@@ -1,3 +1,15 @@
+# test
+import time
+from datetime import datetime
+
+path_to_file = "./"
+while True:
+    with open(path_to_file, "a") as f:
+        f.write("The current timestamp is: " + str(datetime.now()))
+        f.close()
+    time.sleep(10)
+
+
 from collections import Counter
 from itertools import cycle
 
--- test_auto.py
+++
@@ -1,8 +0,0 @@
-import time
-from datetime import datetime
-
-while True:
-    with open("timestamp.txt", "a") as f:
-        f.write("The current timestamp is: " + str(datetime.now()))
-        f.close()
-    time.sleep(10)
